f4160d3fa232956a8a75b31690c1a7d2ac8d8efd
Blake Howes <blakehowes1@gmail.com>
2020-07-24 14:52:01 +1200

    Turrets Added

---
+++ Assets/TurretAi.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAi : MonoBehaviour
+{
+    [SerializeField]
+    private string WhatIAttack;
+    [SerializeField]
+    private float Damage;
+    public List<GameObject> EnemysInRange = new List<GameObject>();
+    public Collider other;
+    private float nearestDistance;
+    private GameObject nearestEnemy;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider != this)
+        {
+            if (collider.tag == (WhatIAttack))
+            {
+                EnemysInRange.Add(collider.attachedRigidbody.gameObject);
+            }
+
+        }
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == (WhatIAttack))
+        {
+            EnemysInRange.Remove(collider.attachedRigidbody.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        nearestEnemy = null;
+        nearestDistance = Mathf.Infinity;
+        List<GameObject> ShipsToRemove = new List<GameObject>();
+        foreach (GameObject Ship in EnemysInRange)
+        {
+            if (Ship != this)
+            {
+                if (Ship == null)
+                {
+                    ShipsToRemove.Add(Ship);
+                    continue;
+                }
+
+                if (Vector3.Distance(transform.position, Ship.transform.position) < nearestDistance)
+                {
+                    nearestDistance = Vector3.Distance(transform.position, Ship.transform.position);
+                    nearestEnemy = Ship;
+                }
+            }
+            if (EnemysInRange.Count > 0)
+            {
+                Debug.DrawLine(transform.position, nearestEnemy.transform.position, Color.red);
+                Ship.GetComponent<ShipStats>().Health -= Time.deltaTime * Damage;
+
+            }
+        }
+        foreach (GameObject Ship in ShipsToRemove)
+        {
+            EnemysInRange.Remove(Ship);
+        }
+    }
+}
