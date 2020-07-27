12204c92b26ccab7acd1dd9eee7639ece7ca6881
Dan <DucatiDan@outlook.com>
2020-07-27 13:26:39 +1200

    Main Menu

---
+++ Assets/SceneCon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCon : MonoBehaviour
+{
+    
+   // Start is called before the first frame update
+    void Awake()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("InGame");
+        }
+    }
+}
