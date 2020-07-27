3956e93e01fbbcc45f780861becc4b02cd07f743
Blake Howes <blakehowes1@gmail.com>
2020-07-27 15:06:10 +1200

    Polish

---
+++ Assets/PlanetRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotate : MonoBehaviour
+{
+    public float Xspeed;
+    public float Yspeed;
+    public float Zspeed;
+    void FixedUpdate()
+    {
+        transform.Rotate((1f* Xspeed) * Time.deltaTime, (1f * Yspeed) * Time.deltaTime, (1f * Zspeed) * Time.deltaTime);
+    }
+}
--- Assets/SceneCon.cs
+++ Assets/SceneCon.cs
@@ -1,23 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneCon : MonoBehaviour
 {
-    
-   // Start is called before the first frame update
-    void Awake()
+    private float timer;
+    public SpriteRenderer Startt;
+    public SpriteRenderer Instructions;
+    public SpriteRenderer Exit;
+    public bool startbutton;
+    public bool startinstructions;
+    public bool startExit;
+    public Button Startbutton;
+    void Start()
     {
-        SceneManager.LoadScene("MainMenu");
+        Startt.enabled = false;
+        Instructions.enabled = false;
+        Exit.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        timer += Time.deltaTime;
+
+        if(timer > 3)
         {
+            if(Startt != null)
+            {
+                Startt.enabled = true;
+            }
+            if (Instructions != null)
+            {
+                Instructions.enabled = true;
+            }
+            if (Exit != null)
+            {
+                Exit.enabled = true;
+            }
+        }
+        if (startbutton == true)
+        {
+            startbutton = false;
             SceneManager.LoadScene("InGame");
         }
+
+        if (startinstructions == true)
+        {
+            startinstructions = false;
+            SceneManager.LoadScene("Instructions");
+        }
+
+        if (startExit == true)
+        {
+            startExit = false;
+            Application.Quit();
+        }
+    }
+    public void StartGame()
+    {
+        startbutton = true;
+    }
+    public void StartInstructions()
+    {
+        startinstructions = true;
+    }
+
+    public void StartExit()
+    {
+        startExit = true;
     }
 }
